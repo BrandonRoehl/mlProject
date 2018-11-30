28eb6dba89cee4fb2afbb81c89207b6268a4e460
Brandon Roehl <SkyGuy913@gmail.com>
2018-11-30 13:49:07 -0600

    numpy array

--- main.py
+++ main.py
@@ -1,55 +1,9 @@
-import csv
 import numpy as np
 
 
-class Patient(object):
-    age = 0
-    sex = 0
-    cp = 0
-    trestbps = 0
-    chol = 0
-    fbs = 0
-    restecg = 0
-    thalach = 0
-    exang = 0
-    oldpeak = 0
-    slope = 0
-    ca = 0
-    thal = 0
-    num = 0
-
-    # The class "constructor" - It's actually an initializer
-    def __init__(self, age, sex, cp, trestbps, chol, fbs, restecg, thalach, exang, oldpeak, slope, ca, thal, num):
-       self.age = age
-       self.sex = sex
-       self.cp = cp
-       self.trestbps = trestbps
-       self.chol = chol
-       self.fbs = fbs
-       self.restecg = restecg
-       self.thalach = thalach
-       self.exang = exang
-       self.oldpeak = oldpeak
-       self.slope = slope
-       self.ca = ca
-       self.thal = thal
-       self.num = num
-
-    def __repr__(self):
-        return "Patent Age: " + self.age + " Num: " + self.num
-
-
 if __name__ == '__main__':
-    with open('processed.cleveland.data') as csv_file:
-        csv_reader = csv.reader(csv_file, delimiter=',')
-        line_count = 0
-        pats = []
-        for row in csv_reader:
-            pats.append(Patient(*row))
-            line_count += 1
-        print(f'Processed {line_count} lines.')
-        patients = np.array(pats)
-        print(patients)
+    patients = np.genfromtxt('processed.cleveland.data', delimiter=',')
+    print(patients)
 
     print("hello world")
 
