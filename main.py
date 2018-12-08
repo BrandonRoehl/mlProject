42fa53e6d1b58f16832a6af7b69ac810a0dec0b9
Dave Schmitz <g1phonedave@gmail.com>
2018-12-08 16:29:13 -0600

    ship snip on my corn knob

--- main.py
+++ main.py
@@ -27,10 +27,10 @@ if __name__ == '__main__':
     x_train, x_test, y_train, y_test = train_test_split(patients[x], patients[y], random_state=0)
 
     colT = ColumnTransformer(
-        [("dummy_col", OneHotEncoder(categories=[[0, 1],
-                                                 [1, 2, 3, 4],
-                                                 [0, 1, 2],
-                                                 [1, 2, 3]]), [1, 2, 6, 10]),
+        [("onehot", OneHotEncoder(categories=[[0, 1],
+                                              [1, 2, 3, 4],
+                                              [0, 1, 2],
+                                              [1, 2, 3]]), [1, 2, 6, 10]),
          ("norm", Normalizer(norm='l1'), [0, 3, 4, 5, 7, 8, 9, 11])])
 
     x_train = colT.fit_transform(x_train)
@@ -47,20 +47,6 @@ if __name__ == '__main__':
     y_train = to_categorical(y_train)
     y_test = to_categorical(y_test)
     n_output = y_train.shape[1]
-    #
-    # model = tf.keras.Sequential()
-    # model.add(layers.Dense(n_hidden, activation='sigmoid', input_shape=(n_input,)))
-    # model.add(layers.Dense(n_output, activation='sigmoid'))
-    # model.compile(optimizer=tf.train.AdamOptimizer(learning_rate), loss=tf.keras.losses.categorical_crossentropy,
-    #               metrics=['accuracy'])
-    #
-    # model.summary()
-    #
-    # model.fit(x_train, y_train, epochs=training_epochs, batch_size=n_length, validation_data=(x_test, y_test),
-    #           verbose=2)
-    # score = model.evaluate(x_test, y_test, verbose=0)
-    # print('Loss: ', score[0])
-    # print('Accuracy: ', score[1])
 
     weights = {
         "hidden": tf.Variable(tf.random_normal([n_input, n_hidden]), name="weight_hidden"),
@@ -73,8 +59,8 @@ if __name__ == '__main__':
     }
 
 
-    def model(X, weights, bias):
-        layer_1 = tf.add(tf.matmul(X, weights["hidden"]), bias["hidden"])
+    def model(x, weights, bias):
+        layer_1 = tf.add(tf.matmul(x, weights["hidden"]), bias["hidden"])
         layer_1 = tf.nn.relu(layer_1)
 
         output_layer = tf.matmul(layer_1, weights["output"]) + bias["output"]
