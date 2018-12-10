33e6ccfbecd48a8f1788f4e67c8a441786f21043
Brandon Roehl <SkyGuy913@gmail.com>
2018-12-10 13:47:42 -0600

    Best I can do

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from tensorflow.keras import layers
 if __name__ == '__main__':
     # Hyper parameters
     learning_rate = .001
-    training_epochs = 2000
+    training_epochs = 4000
     display_epochs = 100
     hidden_nodes = 72
     data_type = 'float'
@@ -58,11 +58,11 @@ if __name__ == '__main__':
     n_output = y_train.shape[1]
 
     model = tf.keras.Sequential()
-    model.add(layers.Dense(hidden_nodes, activation=tf.keras.activations.softmax, input_shape=(n_input,)))
-    # model.add(layers.Dropout(0.2))
+    model.add(layers.Dense(hidden_nodes, activation=tf.keras.activations.sigmoid, input_shape=(n_input,)))
+    model.add(layers.Dropout(0.2))
     model.add(layers.Dense(hidden_nodes, activation=tf.keras.activations.relu))
     model.add(layers.Dropout(0.5))
-    model.add(layers.Dense(n_output, activation=tf.keras.activations.hard_sigmoid))
+    model.add(layers.Dense(n_output, activation=tf.keras.activations.softmax))
 
     model.compile(optimizer=tf.train.AdamOptimizer(learning_rate), loss=tf.keras.losses.binary_crossentropy,
                   metrics=['accuracy'])
@@ -74,79 +74,3 @@ if __name__ == '__main__':
     score = model.evaluate(x_test, y_test, verbose=0)
     print('Loss: ', score[0])
     print('Accuracy: ', score[1])
-
-    # weights = {
-    #     "hidden": tf.Variable(tf.random_normal([n_input, hidden_nodes]), name="weight_hidden"),
-    #     "output": tf.Variable(tf.random_normal([hidden_nodes, n_output]), name="weight_output")
-    # }
-    #
-    # bias = {
-    #     "hidden": tf.Variable(tf.random_normal([hidden_nodes]), name="bias_hidden"),
-    #     "output": tf.Variable(tf.random_normal([n_output]), name="bias_output")
-    # }
-    #
-    #
-    # def model(x, weights, bias):
-    #     layer_1 = tf.add(tf.matmul(x, weights["hidden"]), bias["hidden"])
-    #     layer_1 = tf.nn.relu(layer_1)
-    #
-    #     output_layer = tf.matmul(layer_1, weights["output"]) + bias["output"]
-    #     return output_layer
-    #
-    #
-    # X = tf.placeholder(data_type, [None, n_input])
-    # Y = tf.placeholder(data_type, [None, n_output])
-    # pred = model(X, weights, bias)
-    #
-    # # Define loss and optimizer
-    # cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits=pred, labels=Y))
-    # optimizer = tf.train.AdamOptimizer(learning_rate).minimize(cost)
-    #
-    # # Initializing global variables
-    # init = tf.global_variables_initializer()
-    #
-    # with tf.Session() as sess:
-    #     sess.run(init)
-    #
-    #     for epoch in range(training_epochs):
-    #         _, c = sess.run([optimizer, cost], feed_dict={X: x_train, Y: y_train})
-    #         if (epoch + 1) % display_epochs == 0:
-    #             # a = 5
-    #             print("Epoch: ", (epoch + 1), " Cost: ", c)
-    #     print("Optimization finished!")
-    #
-    #     # Get train and test results and compare them to the expected values
-    #
-    #     # Train results
-    #     train_result = sess.run(pred, feed_dict={X: x_train})
-    #     correct_pred_train = tf.equal(tf.argmax(train_result, 1), tf.argmax(y_train, 1))
-    #     sess.run(correct_pred_train)
-    #     # print(train_result)
-    #     # print(np.argmax(train_result, 1))
-    #
-    #     # Test results
-    #     test_result = sess.run(pred, feed_dict={X: x_test})
-    #     correct_pred_test = tf.equal(tf.argmax(test_result, 1), tf.argmax(y_test, 1))
-    #     sess.run(correct_pred_test)
-    #     # print(test_result)
-    #     # print(np.argmax(test_result, 1))
-    #
-    #     # Calculate the accuracy for each dataset
-    #     train_accuracy = tf.reduce_mean(tf.cast(correct_pred_train, data_type))
-    #     test_accuracy = tf.reduce_mean(tf.cast(correct_pred_test, data_type))
-    #
-    #     print(sess.run(train_accuracy))
-    #     print(sess.run(test_accuracy))
-    #
-    #     print("Train Accuracy: ", train_accuracy.eval({X: x_train, Y: y_train}))
-    #     print("Test Accuracy: ", test_accuracy.eval({X: x_test, Y: y_test}))
-    #
-    #     """"""
-    #     print("")
-    #     # tvars = tf.trainable_variables()
-    #     # tvars_vals = sess.run(tvars)
-    #     #
-    #     # for var, val in zip(tvars, tvars_vals):
-    #     #     print(var.name, val)
-    #
-    #     sess.close()
